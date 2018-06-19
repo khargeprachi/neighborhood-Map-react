4df161d04cb9299100cf43ee9207cbdcfda4c7bf
khargeprachi <kharge.prachi@gmail.com>
2018-06-20 00:54:53 +0530

    Update service-worker.js

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,49 +1,7 @@
-//"use strict";
-/*var precacheConfig=[["/index.html","aa885ba4da1b5cbbccf1fd24f485881f"],["/static/css/main.e1dd37d6.css","463c7816d72f3724c365684d3e5addd0"],["/static/js/main.4eac105f.js","2b9ded243d9029976810eeadc50e7bd1"],["/static/js/App.js"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);
+"use strict";
+var precacheConfig=[["/index.html","aa885ba4da1b5cbbccf1fd24f485881f"],["/static/css/main.e1dd37d6.css","463c7816d72f3724c365684d3e5addd0"],["/static/js/main.4eac105f.js","2b9ded243d9029976810eeadc50e7bd1"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);
   return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob())
   .then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,/\.\w{8}\./);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(r){return setOfCachedUrls(r).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return r.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),r="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,r),e=urlsToCacheKeys.has(n));var a="/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
-*/
 
-let urlsToCache=[
-  '/',
-  '/static/js/App.js',
-  '/static/js/list.js',
-  '/static/js/map.js',
-  '/static/css/App.css',
-  '/static/css/index.css',
-  '/static/js/InfoAPI.js',
-  '/static/js/index.js'
-];
-self.addEventListener('install', function (event) {
 
-    event.waitUntil(
-        caches.open('cache1').then((cache) => {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-addToCache = (name,request,response) => {
-  caches.open('cache1').then(cache => cache.put(request,response))
-}
-self.addEventListener( "fetch" ,(event) => {
-    //fetch request as specified by event object
-//console.log(event.request);
-    event.respondWith(
-        fetch(event.request)
-        .then((response) => {
-          console.log(response);
 
-                urlsToCache.push(event.request);
-                addToCache('cache1',event.request,response.clone());
-                return response;
-        })
-        .catch( (error)=> {
-          console.log(' catch net');
-          return caches.match(event.request).then(function(response) {
-            return response;
-          })
-        })
-    );
-});
-
-    //console.log('hello'); //Note that Request and Response are also objects
